fe7e5dc2990ae7185421ce52d886437b97491afa
Mike Baldwin <mikebaldwin21@gmail.com>
2018-01-20 11:17:27 -0500

    Forgot to format the class to the required style.

--- src/UnitPricing.java
+++ src/UnitPricing.java
@@ -1,19 +1,17 @@
 import java.util.Scanner;
 
-public class UnitPricing
-{
-	public static void main(String[] args)
-	{
-		Scanner inputStream = new Scanner(System.in);
-		int quantity;
-		int unitPrice;
+public class UnitPricing {
+    public static void main(String[] args) {
+        Scanner inputStream = new Scanner(System.in);
+        int quantity;
+        int unitPrice;
 
-		System.out.print("Please enter the Quantity desired:");
-		quantity = inputStream.nextInt();
-		System.out.print("Please enter the Unit price:");
-		unitPrice = inputStream.nextInt();
-		System.out.println("The Quantity desired is: " + quantity);
-		System.out.println("The Unit Price is: $" + unitPrice);
-		System.out.println("The Total Amount is: $" + (quantity * unitPrice));
-	}
+        System.out.print("Please enter the Quantity desired:");
+        quantity = inputStream.nextInt();
+        System.out.print("Please enter the Unit price:");
+        unitPrice = inputStream.nextInt();
+        System.out.println("The Quantity desired is: " + quantity);
+        System.out.println("The Unit Price is: $" + unitPrice);
+        System.out.println("The Total Amount is: $" + (quantity * unitPrice));
+    }
 }
